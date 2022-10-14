41817c1cfdb51b0163ddbbc0f0f863c0159c1d22
yanis <yanis.bouger@gmail.com>
2022-10-14 08:47:42 +0200

    modif de app

--- tpmaven/src/main/java/fr/esir/mdi/ci/tpmaven/App.java
+++ tpmaven/src/main/java/fr/esir/mdi/ci/tpmaven/App.java
@@ -6,7 +6,7 @@ package fr.esir.mdi.ci.tpmaven;
  */
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello Worlfrefrferfred!");
   }
   
   
